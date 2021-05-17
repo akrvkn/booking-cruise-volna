75b4a17ae33d502e17cc26b188e1a0b5d2396bac
akrvkn <akrvkn@yandex.ru>
2021-05-17 14:18:44 +0300

    api mobile

--- assets/js/ships-list.js
+++ assets/js/ships-list.js
@@ -14,20 +14,36 @@ $.when(mtf_ships, mtf_ships_images, ships, ships_images)
     .done(function(mtf_ships, mtf_ships_images, ships, ships_images) {
         // Executed when both requests complete successfully
         // Both results are available here
-        renderShipsList(mtf_ships[0], mtf_ships_images[0], 'mtf');
+        renderMtfShipsList(mtf_ships[0], mtf_ships_images[0], 'mtf');
         renderShipsList(ships[0], ships_images[0], 'inf');
+        //console.log(ships[0]);
     })
     .fail(function() {
         console.log('error');
     });
-
-function renderShipsList(ships, images, com){
+function renderMtfShipsList(ships, images, com){
     return $.map(ships, function(ship, i) {
         if(images[i]) {
             var li = $('<li>'),
                 img = $('<img>', {'src': images[i], 'width': 150}),
                 span = $('<span>'),
                 a = $('<a>', { 'href': '/ship?com=' + com + '&ship=' + i, 'html': ship});
+            span.append(a);
+            li.append(img);
+            li.append(span);
+            ul.append(li);
+        }
+    });
+}
+
+function renderShipsList(ships, images, com){
+    return $.map(ships, function(ship, i) {
+        console.log(ship);
+        if(images[i] && ship['company'] === 'iff') {
+            var li = $('<li>'),
+                img = $('<img>', {'src': images[i], 'width': 150}),
+                span = $('<span>'),
+                a = $('<a>', { 'href': '/ship?com=' + ship['company'] + '&ship=' + ship['shipid'], 'html': ship['shipname']});
             span.append(a);
             li.append(img);
             li.append(span);
